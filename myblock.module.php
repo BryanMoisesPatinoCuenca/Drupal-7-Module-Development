2745402a87d55ca65f0812cfc5e89c916733ef7a
Bryan Patiño <bryan.m.pt@hotmail.com>
2022-02-03 06:56:57 -0500

    Uso de hook_menu y hook_permission

--- myblock.module.php
+++ myblock.module.php
@@ -1,14 +1,45 @@
 <?php
 
-/**
+ /**
   * @file
   * myblock.module
   */
+
+  /**
+  * Implements hook_permission().
+  */
+function myblock_permission() {
+  return array(
+    'acces mypage' => array(
+      'title' => t('Access My Page'),
+      'description' => t('Access My Page on my site.'),
+      )
+  );
+  return $items;
+}
+  /**
+  * Implements hook_menu().
+  */
+  function myblock_menu() {
+    $items['mypage'] = array(
+      'title' => 'My  Page',
+      'type' => MENU_NORMAL_ITEAM,
+      'page callback' => 'myblock_mypage' ,
+      'access arguments' => array('access content')
+  );
+  return $items;
+}
  /**
+  * My Callback function
+  */
+ function myblock_mypage() {
+  return "<strong>This is a test<strong>";
+}
+
+  /**
   * Implements hook_block_info().
   */
-function myblock_block_info()
-{
+function myblock_block_info(){
   $blocks['myblock'] = array(
     'info' => t('My Custom Block'),
   );
